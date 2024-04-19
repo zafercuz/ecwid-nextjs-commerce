b080326301c28005a1efa17ef2c5abe33156cd44
meteor <meteor@ecwid.com>
2024-04-19 19:22:33 +0400

    Improved using CartModal on checkout page

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,8 +1,8 @@
-import Navbar from 'components/layout/navbar';
+import NavbarWithoutCartIcon from 'components/layout/navbar/index-without-cart-icon';
 import { GeistSans } from 'geist/font';
 import { ensureStartsWith } from 'lib/utils';
 import { ReactNode, Suspense } from 'react';
-import './globals.css';
+import '../globals.css';
 
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -35,7 +35,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     return (
         <html lang="en" className={GeistSans.variable}>
             <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-                <Navbar />
+                <NavbarWithoutCartIcon />
                 <Suspense>
                     <main>{children}</main>
                 </Suspense>
